21f21d7cb1ed19aeff6b72652829cdd645273dad
MathiasBergz <mathias15052004@gmail.com>
2025-04-08 18:00:01 -0300

    Created Post component and implemented on App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import {Header} from "./components/Header.jsx";
 
 import './global.css';
 import styles from './App.module.css';
+import {Post} from "./Post.jsx";
 
 function App() {
 
   return (
       <div>
         <Header/>
-        <div className="wrapper">
-
+        <div className={styles.wrapper}>
+            <aside>
+                SideBar
+            </aside>
+            <main>
+                <Post
+                    author="Mathias Bergmann"
+                    content="Text sample"
+                />
+            </main>
         </div>
       </div>
   )
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+﻿export function Post(props) {
+    return (
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </div>
+    );
+}
