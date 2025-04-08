74a2d45437b7a10a13ff56f1d5d78fa9e2bb4c6d
MathiasBergz <mathias15052004@gmail.com>
2025-04-08 17:38:05 -0300

    Created header component with their style

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+﻿import styles from './Header.module.css';
+
+import logo from '../assests/logo.svg';
+export function Header() {
+    return (
+      <header className={styles.header}>
+          <img src={logo} alt="Ignite Logo"/>
+      </header>
+    );
+}
