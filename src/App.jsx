ceedd8ec65d8671a81f3792c8853c8b9e504589c
MathiasBergz <mathias15052004@gmail.com>
2025-04-08 17:37:02 -0300

    Created React project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {Header} from "./components/Header.jsx";
+
+import './global.css';
+import styles from './App.module.css';
+
+function App() {
+
+  return (
+      <div>
+        <Header/>
+        <div className="wrapper">
+
+        </div>
+      </div>
+  )
+}
+
+export default App
